029de941e9a0b4d2b0e3894e172fd9b3c4792dc8
jonwesneski <jonwes2@gmail.com>
2025-06-14 12:58:16 -0500

    updating linting stuff

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,15 +1,15 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+const js = require("@eslint/js");
+const eslintConfigPrettier = require("eslint-config-prettier");
+const turboPlugin = require("eslint-plugin-turbo");
+const tseslint = require("typescript-eslint");
+const onlyWarn = require("eslint-plugin-only-warn");
 
 /**
  * A shared ESLint configuration for the repository.
  *
  * @type {import("eslint").Linter.Config[]}
- * */
-export const config = [
+ */
+module.exports = [
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
