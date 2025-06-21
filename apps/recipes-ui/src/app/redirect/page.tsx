49bbd2c23d527df616703f3acd06992f6dc60f54
jonwesneski <jonwes2@gmail.com>
2025-06-21 09:49:15 -0500

    test something

--- apps/recipes-ui/src/app/redirect/page.tsx
+++ apps/recipes-ui/src/app/redirect/page.tsx
@@ -1,10 +1,25 @@
 import { cookies } from 'next/headers'
-import { ClientRedirect } from './_clientRedirect'
+import React from 'react'
 
 const Page = async () => {
   const cookieStore = await cookies()
   const myCookie = cookieStore.get('access_token')
+  cookieStore.getAll()
   console.log('PAGE', myCookie)
-  return <ClientRedirect accessToken={myCookie?.value} />
+
+  return (
+    <>
+      {cookieStore.getAll().map((value) => {
+        return (
+          <React.Fragment key={value.name}>
+            <p>
+              {value.name} {value.value}
+            </p>
+          </React.Fragment>
+        )
+      })}
+      {/* <ClientRedirect accessToken={myCookie?.value} /> */}
+    </>
+  )
 }
 export default Page
