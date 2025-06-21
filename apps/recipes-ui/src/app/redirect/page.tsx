4422c9e3b76593c6e83f7e588828bff25a79143f
jonwesneski <jonwes2@gmail.com>
2025-06-21 17:10:04 -0500

    trying without serverside component

--- apps/recipes-ui/src/app/redirect/page.tsx
+++ apps/recipes-ui/src/app/redirect/page.tsx
@@ -1,10 +1,35 @@
-import { cookies } from 'next/headers'
-import { ClientRedirect } from './_clientRedirect'
+'use client'
+// import { cookies } from 'next/headers'
+// import { ClientRedirect } from './_clientRedirect'
 
-const Page = async () => {
-  const cookieStore = await cookies()
-  const myCookie = cookieStore.get('access_token')
+// const Page = async () => {
+//   const cookieStore = await cookies()
+//   const myCookie = cookieStore.get('access_token')
 
-  return <ClientRedirect accessToken={myCookie?.value} />
+//   return <ClientRedirect accessToken={myCookie?.value} />
+// }
+// export default Page
+'use client'
+
+import { redirect, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { useAuthentication } from '../../providers/authentication-provider'
+
+const ClientRedirect = () => {
+  const { accessToken, setAccessToken } = useAuthentication()
+  const searchParams = useSearchParams()
+  const queryToken = searchParams.get('token')
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (accessToken) {
+        redirect('/recipes')
+      } else if (queryToken) {
+        setAccessToken(queryToken)
+      }
+    }
+  }, [accessToken, setAccessToken])
+
+  return null
 }
-export default Page
+export default ClientRedirect
