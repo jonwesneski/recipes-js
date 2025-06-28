872f08e02548b5a34af39bac356b42dfc37d0447
jonwesneski <jonwes2@gmail.com>
2025-06-28 16:55:27 -0500

    fix: nav

--- apps/recipes-ui/src/components/navigation/Navbar.tsx
+++ apps/recipes-ui/src/components/navigation/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useUsersControllerUpdateUserV1 } from '@repo/recipes-codegen/users'
 import { useUserStore } from '@src/providers/use-store-provider'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const Navbar = () => {
   const { handle, useDarkMode } = useUserStore((state) => state)
@@ -11,6 +11,7 @@ export const Navbar = () => {
   const [lightDarkString, setLightDarkString] = useState<'Light' | 'Dark'>(
     useDarkMode ? 'Light' : 'Dark',
   )
+  const menuRef = useRef<HTMLDivElement>(null)
 
   const { mutate } = useUsersControllerUpdateUserV1()
 
@@ -36,10 +37,23 @@ export const Navbar = () => {
           useDarkMode: newValue,
         },
       })
+      setIsOpen(false)
 
       return newValue
     })
   }
+
+  const handleClick = (e: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+      setIsOpen(false)
+    }
+  }
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClick)
+      return () => document.removeEventListener('mousedown', handleClick)
+    }
+  }, [isOpen])
 
   return (
     <nav className="relative border-2">
@@ -53,7 +67,10 @@ export const Navbar = () => {
           NAVBAR
         </button>
         {isOpen ? (
-          <div className="absolute flex flex-col-reverse -translate-y-18 md:translate-y-5 md:flex-col border-2">
+          <div
+            ref={menuRef}
+            className="absolute flex flex-col-reverse -translate-y-18 md:translate-y-5 md:flex-col border-2"
+          >
             <div
               onClick={handleLightDarkMode}
               role={'button'}
