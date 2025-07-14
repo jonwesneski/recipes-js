a90a8416f658e91f4c3d9fffa82cee4a34596954
Jon Wesneski <jonwes2@gmail.com>
2025-07-14 17:40:51 -0500

    cleanup

--- apps/ui/src/components/recipe/IngredientsTextArea.tsx
+++ apps/ui/src/components/recipe/IngredientsTextArea.tsx
@@ -1,44 +1,30 @@
+/* eslint-disable -- experimental */
 'use client'
 
-import { TextArea } from '@repo/design-system'
 import { useRecipeStepIngredientsStore } from '@src/providers/recipe-store-provider'
-import { IngredientsValidator } from '@src/utils/ingredientsValidator'
-import { type RefObject, useEffect, useRef, useState } from 'react'
+import { IngredientValidator } from '@src/utils/ingredientsValidator'
+import React, { RefObject, useRef, useState } from 'react'
+import { IngredientRow } from './IngredientRow'
 import { IngredientsMeasurementPopUp } from './IngredientsMeasurementPopup'
 
-const placeholder = `0.5 cups fresh basil
-1 1/4 cups peanuts
-3 whole eggs
-1 pinch salt`
-
 interface IngredientsTextAreaProps {
-  ref?: RefObject<HTMLTextAreaElement | null>
+  ref?: RefObject<HTMLDivElement | null>
   onResize: (_height: number) => void
 }
 export const IngredientsTextArea = (props: IngredientsTextAreaProps) => {
   const [isPopupVisible, setIsPopupVisible] = useState(false)
   const [popupPosition, setPopupPosition] = useState({ x: 0, y: 0 })
-  let textAreaRef = useRef<HTMLTextAreaElement>(null)
+  let textAreaRef = useRef<HTMLDivElement>(null)
   textAreaRef = props.ref ?? textAreaRef
   const {
     ingredients,
-    setIngredients,
-    shouldBeFocused,
+    addIngredient,
+    removeIngredient,
+    updateIngredient,
     insertIngredientsSteps,
   } = useRecipeStepIngredientsStore(textAreaRef)
 
-  useEffect(() => {
-    if (textAreaRef.current && shouldBeFocused) {
-      // Set Caret Position
-      textAreaRef.current.focus()
-      textAreaRef.current.setSelectionRange(
-        textAreaRef.current.value.length,
-        textAreaRef.current.value.length,
-      )
-    }
-  }, [])
-
-  const getCaretPosition = (element: HTMLTextAreaElement) => {
+  const getCaretPosition = (element: HTMLInputElement) => {
     const position: { row: number; column: number } = {
       row: 0,
       column: 0,
@@ -46,7 +32,7 @@ export const IngredientsTextArea = (props: IngredientsTextAreaProps) => {
 
     // Calcuate row position
     // // do we care about row position?
-    const cursorPosition = element.selectionStart
+    const cursorPosition = element.selectionStart || 0
     const textBeforeCursor = element.value.substring(0, cursorPosition)
     const row = textBeforeCursor.split('\n').length
 
@@ -60,123 +46,112 @@ export const IngredientsTextArea = (props: IngredientsTextAreaProps) => {
     return position
   }
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setIngredients(
-      new IngredientsValidator({ stringValue: event.target.value }),
-    )
+  const handleChange = (
+    ref: React.RefObject<HTMLTextAreaElement | null>,
+    ingredient: IngredientValidator,
+  ) => {
+    updateIngredient(ref, ingredient)
   }
 
-  const handleMouseUp = (_event: React.MouseEvent<HTMLTextAreaElement>) => {
-    _handleShowPopUp()
+  const handleNewRow = (ref: React.RefObject<HTMLTextAreaElement | null>) => {
+    addIngredient(ref)
   }
 
-  const handleTouchEnd = (_event: React.TouchEvent<HTMLTextAreaElement>) => {
-    _handleShowPopUp()
+  const handleRemove = (ref: React.RefObject<HTMLTextAreaElement | null>) => {
+    removeIngredient(ref)
   }
 
-  const _handleShowPopUp = () => {
-    if (textAreaRef.current) {
-      const rect = textAreaRef.current.getBoundingClientRect()
-      const position = getCaretPosition(textAreaRef.current)
-      // is caret in measurement-unit column
-      if (position.column === 1) {
-        const x = rect.width / 6
-        const y = position.row + 1
-        const yMultipler = y * 10
-        setPopupPosition({
-          x: rect.x - x,
-          y: rect.y + yMultipler + 50,
-        })
-        setIsPopupVisible(true)
-      } else {
-        setIsPopupVisible(false)
-      }
+  const handleArrowUp = (ref: React.RefObject<HTMLTextAreaElement | null>) => {
+    const index = ingredients?.items.findIndex((item) => item.ref === ref)
+    if (index && index > 0) {
+      ingredients?.items[index - 1].ref.current?.focus()
     }
   }
 
-  const handleBlur = () => {
-    setIsPopupVisible(false)
+  const handleArrowDown = (
+    ref: React.RefObject<HTMLTextAreaElement | null>,
+  ) => {
+    const index = ingredients?.items.findIndex((item) => item.ref === ref)
+    if (index !== undefined && index < (ingredients?.items.length || 0) - 1) {
+      ingredients?.items[index + 1].ref.current?.focus()
+    }
   }
 
-  /* When some pastes in recipes that are already separated by '\r\n\r\n'
+  const handleFocus = () => {
+    for (const item of ingredients?.items || []) {
+      if (document.activeElement === item.ref.current) {
+        return
+      }
+    }
+    if (ingredients?.items.length) {
+      ingredients.items[ingredients.items.length - 1].ref.current?.focus()
+    }
+  }
+
+  /* When some pastes in recipes that are already separated by linebreaks
    * we will add new steps for them
    */
-  const handleOnPaste = (event: React.ClipboardEvent<HTMLTextAreaElement>) => {
-    event.preventDefault()
-    const stringData = event.clipboardData.getData('text/plain')
-    const data = stringData.includes('\r')
-      ? stringData.split('\r\n\r\n')
-      : stringData.split('\n\n')
+  const handleOnPaste = (
+    ref: React.RefObject<HTMLTextAreaElement | null>,
+    value: string,
+  ) => {
+    const dataListofList = value.includes('\r')
+      ? value.split('\r\n\r\n').map((v) => v.split('\r\n'))
+      : value.split('\n\n').map((v) => v.split('\n'))
     insertIngredientsSteps(
-      data.map((d) => new IngredientsValidator({ stringValue: d })),
+      ref,
+      dataListofList.map((dl) =>
+        dl.map((d) => new IngredientValidator({ stringValue: d })),
+      ),
     )
   }
 
-  const handleOnInput = (event: React.InputEvent<HTMLTextAreaElement>) => {
-    const descriptors = Object.getOwnPropertyDescriptors(
-      Object.getPrototypeOf(event.nativeEvent),
-    )
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any -- f
-    const obj: Record<string, any> = {}
-    for (const key in descriptors) {
-      if (descriptors[key].get) {
-        obj[key] = event.nativeEvent[key as never]
-        obj[`${key}TYPE`] = typeof event.nativeEvent[key as never]
-      }
-    }
-    console.log(JSON.stringify(obj))
-    // console.log(
-    //   JSON.stringify({
-    //     ...obj,
-    //     ISINSERT: event.nativeEvent.inputType === 'insertText',
-    //     INSERTDATA: event.nativeEvent.data,
-    //   }),
-    // )
-    if (event.nativeEvent.inputType === 'insertLineBreak') {
-      // This might have been pasted from some mobile phones like
-      // android keyboard (which doesn't fire onPaste) so calling it
-      // explicitly here
-      // const end1 = event.currentTarget.textContent?.slice(-4) || ''
-      // const end2 = event.currentTarget.textContent?.slice(-2) || ''
-      // if (end1 === '\r\n' || end2 === '\n') {
-      //   event.preventDefault()
-      //   props.onPaste([new IngredientsValidator({ stringValue: '' })])
-      // }
-      // console.log(
-      //   JSON.stringify({ inputValue: event.currentTarget.textContent }),
-      // )
-    }
-  }
-
-  const handleOnResize = (height: number) => {
-    if (textAreaRef.current) {
-      textAreaRef.current.style.height = `${height}px`
+  const _handleShowPopUp = (row: HTMLInputElement) => {
+    const rect = row.getBoundingClientRect()
+    const position = getCaretPosition(row)
+    // is caret in measurement-unit column
+    if (position.column === 1) {
+      const x = rect.width / 6
+      const y = position.row + 1
+      const yMultipler = y * 10
+      setPopupPosition({
+        x: rect.x - x,
+        y: rect.y + yMultipler + 50,
+      })
+      setIsPopupVisible(true)
+    } else {
+      setIsPopupVisible(false)
     }
   }
 
   return (
-    <>
-      <TextArea
-        ref={textAreaRef}
-        className="min-h-32 min-w-80 grow-1"
-        value={ingredients?.stringValue}
-        placeholder={placeholder}
-        onChange={handleInputChange}
-        onMouseUp={handleMouseUp}
-        onTouchEnd={handleTouchEnd}
-        onBlur={handleBlur}
-        onPaste={handleOnPaste}
-        onInput={handleOnInput}
-        onResize={handleOnResize}
-        data-testid="ingredients-text-area"
-      />
+    <div
+      ref={textAreaRef}
+      data-testid="ingredients-text-area"
+      className="focus-within:bg-input-focus-background"
+      onClick={handleFocus}
+    >
+      {ingredients?.items.map((item) => (
+        <IngredientRow
+          key={item.keyId}
+          ref={item.ref}
+          value={item.ingredient.stringValue}
+          error={item.ingredient.error?.issues[0].message}
+          focusOnMount={item.shouldBeFocused}
+          onChange={handleChange}
+          onPaste={handleOnPaste}
+          onEnterPressed={handleNewRow}
+          onRemove={handleRemove}
+          onArrowDown={handleArrowDown}
+          onArrowUp={handleArrowUp}
+        />
+      ))}
       {isPopupVisible ? (
         <IngredientsMeasurementPopUp
           top={popupPosition.y}
           left={popupPosition.x}
         />
       ) : null}
-    </>
+    </div>
   )
 }
--- apps/ui/src/components/recipe/Steps.tsx
+++ apps/ui/src/components/recipe/Steps.tsx
@@ -4,7 +4,7 @@ import { Button } from '@repo/design-system'
 import { useRecipeStore } from '@src/providers/recipe-store-provider'
 import Image from 'next/image'
 import { useEffect, useState, type RefObject } from 'react'
-import { IngredientsTextArea3 } from './IngredientsTextArea.experimental3'
+import { IngredientsTextArea } from './IngredientsTextArea'
 import { InstructionsTextArea } from './InstructionsTextArea'
 import { PhotoInput } from './PhotoInput'
 
@@ -63,14 +63,10 @@ export const Steps = () => {
           <div key={s.keyId} className="mb-5">
             <h1 className="font-bold">step {index + 1}.</h1>
             <div ref={s.ref} className="flex flex-col md:flex-row">
-              <IngredientsTextArea3
+              <IngredientsTextArea
                 ref={s.ingredients.ref}
                 onResize={(height: number) => handleOnResize(s.ref, height)}
               />
-              {/* <IngredientsTextArea
-                ref={s.ingredientsRef}
-                onResize={(height: number) => handleOnResize(s.ref, height)}
-              /> */}
               <InstructionsTextArea
                 ref={s.instructions.ref}
                 onResize={(height: number) => handleOnResize(s.ref, height)}
