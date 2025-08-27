b4c0cf4d143d2e88d04416a6090096abc437ce1f
Jon Wesneski <jonwes2@gmail.com>
2025-08-26 21:00:26 -0500

    removed debug code

--- apps/api/src/recipes/recipes.controller.ts
+++ apps/api/src/recipes/recipes.controller.ts
@@ -60,7 +60,7 @@ export class RecipesController {
   ): Promise<RecipeEntity> {
     let userId: string | undefined;
     try {
-      if (query.byOwner || true) {
+      if (query.byOwner) {
         // TODO: Remove when this works in jest: JwtDecodedHeader.
         userId = parseHelper(request).sub;
       }
