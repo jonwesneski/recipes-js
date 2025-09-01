34f7f883d835546fb810b417844bee232ad89ee4
Jon Wesneski <jonwes2@gmail.com>
2025-09-01 11:09:00 -0500

    fixed height of navbar

--- apps/ui/src/components/navigation/NavLink.tsx
+++ apps/ui/src/components/navigation/NavLink.tsx
@@ -7,7 +7,7 @@ interface INavLinkProps {
 export const NavLink = (props: INavLinkProps) => {
   return (
     <Link href={props.href}>
-      <props.svgIcon className="my-1 mx-1 grow-0 w-6 fill-text" />
+      <props.svgIcon className="my-1 mx-1 grow-0 w-6 h-6 fill-text" />
     </Link>
   )
 }
