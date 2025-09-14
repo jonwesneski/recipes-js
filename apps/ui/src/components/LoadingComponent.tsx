6db3559398b309cce524cebb2c5e8547f3073c86
Jon Wesneski <jonwes2@gmail.com>
2025-09-14 14:15:55 -0500

    chore: test

--- apps/ui/src/components/LoadingComponent.tsx
+++ apps/ui/src/components/LoadingComponent.tsx
@@ -1,72 +1,72 @@
 'use client'
 
-import { useHealthCheckControllerStatus } from '@repo/codegen/healthCheck'
-import { usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
+// import { useHealthCheckControllerStatus } from '@repo/codegen/healthCheck'
+// import { usePathname } from 'next/navigation'
+// import { useEffect, useState } from 'react'
 
-const CenterDiv = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex flex-col items-center justify-center w-full h-screen">
-    {children}
-  </div>
-)
+// const CenterDiv = ({ children }: { children: React.ReactNode }) => (
+//   <div className="flex flex-col items-center justify-center w-full h-screen">
+//     {children}
+//   </div>
+// )
 
 const Loading = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname()
-  const skipApiCall = pathname === '/redirect'
+  // const pathname = usePathname()
+  // const skipApiCall = pathname === '/redirect'
 
-  const { isFetching, error, isSuccess } = useHealthCheckControllerStatus({
-    query: { enabled: !skipApiCall },
-  })
-  let timer: ReturnType<typeof setTimeout>
+  // const { isFetching, error, isSuccess } = useHealthCheckControllerStatus({
+  //   query: { enabled: !skipApiCall },
+  // })
+  // let timer: ReturnType<typeof setTimeout>
 
-  const [assumeServerIsProvisioning, setAssumeServerIsProvisioning] =
-    useState(false)
+  // const [assumeServerIsProvisioning, setAssumeServerIsProvisioning] =
+  //   useState(false)
 
-  useEffect(() => {
-    timer = setTimeout(() => {
-      setAssumeServerIsProvisioning(true)
-    }, 8000)
-    return () => clearTimeout(timer)
-  }, [])
+  // useEffect(() => {
+  //   timer = setTimeout(() => {
+  //     setAssumeServerIsProvisioning(true)
+  //   }, 8000)
+  //   return () => clearTimeout(timer)
+  // }, [])
 
-  useEffect(() => {
-    if (error && error.code !== 'ERR_NETWORK') {
-      console.error('Health check failed:', error)
-    } else if (error) {
-      console.error('some other error:', error)
-    }
-  }, [error])
+  // useEffect(() => {
+  //   if (error && error.code !== 'ERR_NETWORK') {
+  //     console.error('Health check failed:', error)
+  //   } else if (error) {
+  //     console.error('some other error:', error)
+  //   }
+  // }, [error])
 
-  useEffect(() => {
-    if (isSuccess) {
-      clearTimeout(timer)
-      setAssumeServerIsProvisioning(false)
-    }
-  }, [isSuccess])
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     clearTimeout(timer)
+  //     setAssumeServerIsProvisioning(false)
+  //   }
+  // }, [isSuccess])
 
-  if (isFetching) {
-    return (
-      <CenterDiv>
-        <h2 className="font-bold">Loading...</h2>
-        {assumeServerIsProvisioning ? (
-          <h2 className="font-bold">
-            The server is getting ready, it may take up to 5 minutes. Thank you
-            for your patience!
-          </h2>
-        ) : null}
-      </CenterDiv>
-    )
-  }
+  // if (isFetching) {
+  //   return (
+  //     <CenterDiv>
+  //       <h2 className="font-bold">Loading...</h2>
+  //       {assumeServerIsProvisioning ? (
+  //         <h2 className="font-bold">
+  //           The server is getting ready, it may take up to 5 minutes. Thank you
+  //           for your patience!
+  //         </h2>
+  //       ) : null}
+  //     </CenterDiv>
+  //   )
+  // }
 
-  if (error) {
-    return (
-      <CenterDiv>
-        <h2 className="font-bold">
-          Having trouble connecting to server. Please try again later.
-        </h2>
-      </CenterDiv>
-    )
-  }
+  // if (error) {
+  //   return (
+  //     <CenterDiv>
+  //       <h2 className="font-bold">
+  //         Having trouble connecting to server. Please try again later.
+  //       </h2>
+  //     </CenterDiv>
+  //   )
+  // }
 
   return <>{children}</>
 }
