b197ec675946bfb20505a3c9c13dd19ad4def7d6
Jon Wesneski <jonwes2@gmail.com>
2025-09-22 17:35:49 -0500

    added errors and linting

--- packages/design-system/src/Text/TextLabel.tsx
+++ packages/design-system/src/Text/TextLabel.tsx
@@ -16,12 +16,16 @@ interface TextLabelProps {
   onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 export const TextLabel = (props: TextLabelProps) => {
-  const ref = props.ref ?? useRef<HTMLInputElement>(null)
+  const localRef = useRef<HTMLInputElement>(null)
+  const ref = props.ref ?? localRef
   const [isFocused, setIsFocused] = useState(false)
   const [isHovered, setIsHovered] = useState(false)
 
   return (
     <div className="relative">
+      {props.isRequired ? (
+        <span className="absolute -left-3 text-red-900">*</span>
+      ) : null}
       <Text
         ref={ref}
         id={props.name}
@@ -31,7 +35,10 @@ export const TextLabel = (props: TextLabelProps) => {
         onChange={props.onChange}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
-        className={mergeCss({ 'scale-110': isHovered })}
+        className={mergeCss({
+          'scale-110': isHovered,
+          'border-red-900': props.error,
+        })}
         value={props.value}
       />
       <Label
@@ -41,6 +48,17 @@ export const TextLabel = (props: TextLabelProps) => {
           'absolute left-3 top-2 transition-all cursor-text text-text/35',
           { 'text-xs top-0': isFocused || props.value },
         )}
+        onClick={() => {
+          setIsFocused(true)
+          ref.current?.focus()
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
+      <Label
+        htmlFor={props.name}
+        text={props.error ?? ''}
+        className="text-red-900 block"
         onClick={() => {
           setIsFocused(true)
           ref.current?.focus()
