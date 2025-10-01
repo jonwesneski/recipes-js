e74193dd83a5b71f73b22b99cc854d882fa24607
Jon Wesneski <jonwes2@gmail.com>
2025-10-01 17:36:21 -0500

    updated input border and step-photo margin

--- packages/design-system/src/Text/Text.tsx
+++ packages/design-system/src/Text/Text.tsx
@@ -28,7 +28,7 @@ export const Text = ({ variant = 'ellipses', ...props }: TextProps) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       className={mergeCss(
-        'px-2 py-2 align-text-bottom border-2 focus:border-4 focus:outline-none focus:transform focus:origin-left focus:scale-x-105 focus:scale-y-110',
+        'px-2 py-2 align-text-bottom border-2 focus:border-[3px] focus:outline-none focus:transform focus:origin-left focus:scale-x-105 focus:scale-y-110',
         { 'transform origin-left scale-x-105 scale-y-110': isHovered },
         props.className,
       )}
