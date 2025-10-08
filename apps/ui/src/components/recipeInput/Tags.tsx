488b20e6918c39e2dcdf769cf3654a0b037a9437
Jon Wesneski <jonwes2@gmail.com>
2025-10-08 15:20:16 -0500

    clean up

--- apps/ui/src/components/recipeInput/Tags.tsx
+++ apps/ui/src/components/recipeInput/Tags.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { useAiControllerTagsV1 } from '@repo/codegen/ai'
-import { TextButton } from '@repo/design-system'
+import { MultiSelect, TextButton } from '@repo/design-system'
 import useTags from '@src/hooks/useTags'
 import { useRecipeStore } from '@src/providers/recipe-store-provider'
-import AwesomeDebouncePromise from 'awesome-debounce-promise'
 import { useState } from 'react'
 import type { GroupBase, MultiValue, OptionsOrGroups } from 'react-select'
-import Select from 'react-select/async-creatable'
 
 type OptionType = { value: string; label: string }
 
@@ -42,10 +40,6 @@ export const Tags = () => {
     setOptions((value) => [...tagOptions, ...value])
     return tagOptions
   }
-  const debouncedHandleLoadOptions = AwesomeDebouncePromise(
-    handleLoadOptions,
-    500,
-  )
 
   return (
     <section>
@@ -55,26 +49,10 @@ export const Tags = () => {
         text="auto generate tags"
         onClick={handleOnAutoGenerate}
       />
-      <Select
-        isMulti
+      <MultiSelect
         options={options}
         onChange={handleChange}
-        placeholder="Select or type a new option..."
-        isClearable
-        isSearchable
-        cacheOptions
-        defaultOptions
-        loadOptions={debouncedHandleLoadOptions}
-        unstyled
-        classNames={{
-          container: () =>
-            'border-2 hover:scale-105 focus:scale-105 [&_input:focus]:ring-0 focus:ring-offset-0 pb-1',
-          menu: () => 'border rounded-none shadow-lg mt-1',
-          input: () => 'ml-1',
-          multiValue: () => 'border border-dotted ml-1 mt-1',
-          multiValueLabel: () => 'ml-1',
-          multiValueRemove: () => 'text-gray-500 hover:text-gray-700 ml-1',
-        }}
+        onLoadOptions={handleLoadOptions}
       />
     </section>
   )
