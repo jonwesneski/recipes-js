10308e74f9f53eea135b40a807356211ca3539f1
Jon Wesneski <jonwes2@gmail.com>
2025-12-14 14:10:27 -0600

    fix: renaming and fixing typos

---
+++ apps/ui/src/components/recipeInput/ServingsAndNutritionalFacts.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import { useAiControllerNutritionalFactsV1 } from '@repo/codegen/ai'
+import type { RecipeResponseServingUnit } from '@repo/codegen/model'
+import { SelectLabel, TextButton, TextLabel } from '@repo/design-system'
+import { useRecipeStore } from '@src/providers/recipe-store-provider'
+import {
+  measurementUnitsAbbreviated,
+  measurementUnitsPlural,
+} from '@src/utils/measurements'
+import { type ChangeEvent } from 'react'
+import { NutritionalFactsInput } from '../NutritionalFactsInput'
+
+export const ServingsAndNutritionalFacts = () => {
+  const {
+    nutritionalFacts,
+    setNutritionalFacts,
+    setPartialNutritionalFacts,
+    servingUnit,
+    setServingUnit,
+    servingAmount,
+    setServingAmount,
+    servings,
+    setServings,
+    makeGenerateNutritionalFactsDto,
+  } = useRecipeStore((state) => state)
+  const { mutate } = useAiControllerNutritionalFactsV1({
+    mutation: { retry: false },
+  })
+
+  const handleOnAutoGenerate = () => {
+    mutate(
+      { data: makeGenerateNutritionalFactsDto() },
+      {
+        onSuccess: (data) => {
+          setNutritionalFacts(data)
+        },
+      },
+    )
+  }
+
+  const handleOnServingAmountChange = (
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    if (event.target.value) {
+      setServingAmount(parseInt(event.target.value))
+    } else {
+      setServingAmount(null)
+    }
+  }
+
+  const handleOnServingUnitChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    if (event.target.value) {
+      setServingUnit(event.target.value as RecipeResponseServingUnit)
+    } else {
+      setServingUnit(null)
+    }
+  }
+
+  const handleOnServingsChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value) {
+      setServings(parseInt(event.target.value))
+    } else {
+      setServings(null)
+    }
+  }
+
+  return (
+    <>
+      <TextButton
+        className="mb-5"
+        text="auto generate facts"
+        onClick={handleOnAutoGenerate}
+      />
+      <SelectLabel
+        label="serving unit"
+        id="serving-unit"
+        isRequired={false}
+        options={Object.keys(measurementUnitsPlural).map((u) => {
+          return {
+            label:
+              measurementUnitsPlural[u as keyof typeof measurementUnitsPlural],
+            value: u,
+          }
+        })}
+        onChange={handleOnServingUnitChange}
+      />
+      <TextLabel
+        name="servingAmount"
+        isRequired={false}
+        label={`serving size ${servingUnit ? `(${measurementUnitsAbbreviated[servingUnit]})` : ''}`}
+        type="number"
+        dir="rtl"
+        onChange={handleOnServingAmountChange}
+        value={servingAmount?.toString() ?? ''}
+      />
+      <TextLabel
+        className="mb-5"
+        name="servings"
+        isRequired={false}
+        label="total servings"
+        type="number"
+        dir="rtl"
+        onChange={handleOnServingsChange}
+        value={servings?.toString() ?? ''}
+      />
+      <NutritionalFactsInput
+        nutritionalFacts={nutritionalFacts}
+        onNutritionalFactChange={(data) => setPartialNutritionalFacts(data)}
+      />
+    </>
+  )
+}
---
+++ apps/ui/src/components/recipeInput/StepList.test.tsx
@@ -0,0 +1,181 @@
+import { renderRecipeComponent } from '@src/mocks/renderComponent'
+import { act } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { StepList } from './StepList'
+
+jest.mock('jwt-decode', () => ({
+  default: () => ({}),
+  jwtDecode: () => {
+    return {
+      sub: '1234',
+      email: 'test@email.com',
+      handle: '123',
+    }
+  },
+}))
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+}
+
+Object.defineProperty(global, 'localStorage', { value: localStorageMock })
+localStorageMock.getItem.mockImplementation(() => 'jwtvalue')
+
+const INGREDIENT_ID = 'ingredient-row'
+const INSTRUCTIONS_ID = 'instructions-text-area'
+const STEP_ID = 'step-row'
+
+const ingredientsString = '2 cups flour\n\n1 ounces sugar\n\n2 grams paste'
+const instructionsString = 'my second step\n\nmy third step\n\nmy fourth step'
+
+describe('StepList', () => {
+  describe('On New', () => {
+    it('Initial Load', async () => {
+      const { findAllByTestId } = await renderRecipeComponent(<StepList />)
+      const ingredients = await findAllByTestId(INGREDIENT_ID)
+      const instructions = await findAllByTestId(INSTRUCTIONS_ID)
+      expect(ingredients.length).toBe(1)
+      expect(instructions.length).toBe(1)
+    })
+
+    it('Pasting ingredients: Windows', async () => {
+      const ingredientsStringFromWindows = ingredientsString.replaceAll(
+        '\n\n',
+        '\r\n\r\n',
+      )
+      const dataList = ingredientsStringFromWindows.split('\r\n\r\n')
+      const { findAllByTestId } = await renderRecipeComponent(<StepList />)
+      let ingredients = await findAllByTestId(INGREDIENT_ID)
+
+      act(() => {
+        ingredients[0].focus()
+      })
+      await userEvent.paste(ingredientsStringFromWindows)
+
+      ingredients = await findAllByTestId(INGREDIENT_ID)
+      expect(ingredients.length).toBe(3)
+      ingredients.forEach((i, index) => {
+        expect(i.textContent).toBe(dataList[index])
+      })
+
+      const instructions = await findAllByTestId(INSTRUCTIONS_ID)
+      expect(instructions.length).toBe(3)
+      instructions.forEach((i) => {
+        expect(i.textContent).toBe('')
+      })
+    })
+
+    it('Pasting ingredients: Linux', async () => {
+      const dataList = ingredientsString.split('\n\n')
+      const { findAllByTestId } = await renderRecipeComponent(<StepList />)
+      let ingredients = await findAllByTestId(INGREDIENT_ID)
+
+      act(() => {
+        ingredients[0].focus()
+      })
+      await userEvent.paste(ingredientsString)
+
+      const steps = await findAllByTestId(STEP_ID)
+      expect(steps.length).toBe(3)
+      ingredients = await findAllByTestId(INGREDIENT_ID)
+      ingredients.forEach((i, index) => {
+        expect(i.textContent).toBe(dataList[index])
+      })
+
+      const instructions = await findAllByTestId(INSTRUCTIONS_ID)
+      expect(instructions.length).toBe(3)
+      instructions.forEach((i) => {
+        expect(i.textContent).toBe('')
+      })
+    })
+
+    it('Pasting instructions: Windows', async () => {
+      const instructionStringFromWindows = ingredientsString.replaceAll(
+        '\n\n',
+        '\r\n\r\n',
+      )
+      const dataList = instructionStringFromWindows.split('\r\n\r\n')
+      const { findAllByTestId } = await renderRecipeComponent(<StepList />)
+      let instructions = await findAllByTestId(INSTRUCTIONS_ID)
+
+      act(() => {
+        instructions[0].focus()
+      })
+      await userEvent.paste(instructionStringFromWindows)
+
+      instructions = await findAllByTestId(INSTRUCTIONS_ID)
+      expect(instructions.length).toBe(3)
+      instructions.forEach((i, index) => {
+        expect(i.textContent).toBe(dataList[index])
+      })
+
+      const ingredients = await findAllByTestId(INGREDIENT_ID)
+      expect(ingredients.length).toBe(3)
+      ingredients.forEach((i) => {
+        expect(i.textContent).toBe('')
+      })
+    })
+
+    it('Pasting instructions: Linux', async () => {
+      const dataList = instructionsString.split('\n\n')
+      const { findAllByTestId } = await renderRecipeComponent(<StepList />)
+      let instructions = await findAllByTestId(INSTRUCTIONS_ID)
+
+      act(() => {
+        instructions[0].focus()
+      })
+      await userEvent.paste(instructionsString)
+
+      instructions = await findAllByTestId(INSTRUCTIONS_ID)
+      expect(instructions.length).toBe(3)
+      instructions.forEach((i, index) => {
+        expect(i.textContent).toBe(dataList[index])
+      })
+
+      const ingredients = await findAllByTestId(INGREDIENT_ID)
+      expect(ingredients.length).toBe(3)
+      ingredients.forEach((i) => {
+        expect(i.textContent).toBe('')
+      })
+    })
+
+    it('Pasting uneven steps', async () => {
+      const ingredientsList = ingredientsString.split('\n\n')
+      const instructionsList = instructionsString.split('\n\n')
+      const { findAllByTestId } = await renderRecipeComponent(<StepList />)
+      let ingredients = await findAllByTestId(INGREDIENT_ID)
+
+      act(() => {
+        ingredients[0].focus()
+      })
+      await userEvent.paste(ingredientsString)
+      let instructions = await findAllByTestId(INSTRUCTIONS_ID)
+      act(() => {
+        instructions[1].focus()
+      })
+      await userEvent.paste(instructionsString)
+
+      const steps = await findAllByTestId(STEP_ID)
+      expect(steps.length).toBe(4)
+      ingredients = await findAllByTestId(INGREDIENT_ID)
+      ingredients.forEach((i, index) => {
+        if (index === 3) {
+          expect(i.textContent).toBe('')
+          return
+        }
+        expect(i.textContent).toBe(ingredientsList[index])
+      })
+
+      instructions = await findAllByTestId(INSTRUCTIONS_ID)
+      expect(instructions.length).toBe(4)
+      instructions.forEach((i, index) => {
+        if (index === 0) {
+          expect(i.textContent).toBe('')
+          return
+        }
+        expect(i.textContent).toBe(instructionsList[index - 1])
+      })
+    })
+  })
+})
