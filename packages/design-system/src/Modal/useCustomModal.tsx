06529933169f51f901bda73ed53ebef0cf3c78f8
Jon Wesneski <jonwes2@gmail.com>
2025-12-23 14:52:44 -0600

    fix: toast settings

--- packages/design-system/src/Modal/useCustomModal.tsx
+++ packages/design-system/src/Modal/useCustomModal.tsx
@@ -6,15 +6,27 @@ export function useCustomModal() {
 
   const showModal = <T extends object>(
     id: string,
+    blocking: boolean,
     Component: React.ComponentType<T>,
     props: T,
   ) => {
     const modalRoot = document.getElementById('modal-root')
     if (!modalRoot) throw new Error('Root node not found. Cannot render modal.')
-    const portal = createPortal(<Component {...props} />, modalRoot)
-    // Prevent scrolling while modal is up
-    document.body.style.overflow = 'hidden'
-    addModal({ id, portal })
+    const portal = createPortal(
+      blocking ? (
+        <Component {...props} />
+      ) : (
+        <div className="pointer-events-auto">
+          <Component {...props} />
+        </div>
+      ),
+      modalRoot,
+    )
+    if (blocking) {
+      // Prevent scrolling while modal is up
+      document.body.style.overflow = 'hidden'
+    }
+    addModal({ id, portal, blocking })
   }
 
   const closeModal = () => {
